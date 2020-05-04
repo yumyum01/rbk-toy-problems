dae05c70c5349bf6d305246abf3efb73c6881185
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-05-04 09:09:15 +0300

    Add W5D3 toy problem

---
+++ W5D3/W5D3.js
@@ -0,0 +1,39 @@
+/*
+You were put in charge of ordering for tonight's company get-together, 
+and you were given a data set with people's meal preferences and dietary restrictions. 
+Write a function called orderAVegetarianDish that takes  an array of empoloyee objects, 
+and returns true if at least 1 person is listed "vegetarian" on their "mealPreferences". 
+Otherwise, your function should return false.
+
+var staffA = [
+  {
+    name: "Lia",
+    department: "Human Resources",
+    dietaryRestrictions: [],
+    mealPreferences: "no preferences"
+  },
+  {
+    name: "Sebastian",
+    department: "Engineering",
+    dietaryRestrictions: ["dairy free", "gluten free"],
+    mealPreferences: "vegetarian"
+  },
+  {
+    name: "Ari",
+    department: "Executive",
+    dietaryRestrictions: ["dairy free"],
+    mealPreferences: "vegetarian"
+  },
+  {
+    name: "Martha",
+    department: "Legal",
+    dietaryRestrictions: ["nut allergies"],
+    mealPreferences: "non-vegetarian"
+  }
+];
+Calling your function should result in:
+
+orderAVegetarianDish(staffA); //true
+*/
+
+// your answer is here 
